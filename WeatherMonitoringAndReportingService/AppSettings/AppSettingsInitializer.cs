77d9ef025e0de53ed7eaef74c64437a39df8ebc3
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-09-24 21:43:38 +0300

    App settings configuration

---
+++ WeatherMonitoringAndReportingService/AppSettings/AppSettingsInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringAndReportingService.AppSettings;
+
+internal class AppSettingsInitializer
+{
+    private static AppSettingsModel? _appSettings;
+
+    private AppSettingsInitializer()
+    {
+        string appSettingsJson = File.ReadAllText("../../../AppSettings/appsettings.json");
+        _appSettings = JsonSerializer.Deserialize<AppSettingsModel>(appSettingsJson);
+    }
+
+    public static AppSettingsModel AppSettingsInstance()
+    {
+        if (_appSettings == null) new AppSettingsInitializer();
+
+        return _appSettings;
+    }
+}
---
+++ WeatherMonitoringAndReportingService/AppSettings/AppSettingsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMonitoringAndReportingService.AppSettings;
+
+public class AppSettingsModel
+{
+    public string ConfigFilePath { get; set; }
+}
