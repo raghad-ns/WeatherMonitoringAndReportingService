dc663959817f8dfe096a86957d93cb1f19d3bafe
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-09-25 12:27:07 +0300

    Adding xml adapter

---
+++ WeatherMonitoringAndReportingService/InputConversion/XMLToWeatherDetailsAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using WeatherMonitoringAndReportingService.WeatherDetails;
+
+namespace WeatherMonitoringAndReportingService.InputConversion;
+
+public class XMLToWeatherDetailsAdapter : IAdapter
+{
+    public static WeatherDetailsModel ToWeatherDetailsAdapter(string xml)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(WeatherDetailsModel), new XmlRootAttribute("WeatherData"));
+        using (StringReader reader = new StringReader(xml))
+        {
+            return (WeatherDetailsModel)serializer.Deserialize(reader);
+        }
+    }
+}
--- WeatherMonitoringAndReportingService/Program.cs
+++ WeatherMonitoringAndReportingService/Program.cs
@@ -3,6 +3,7 @@ using WeatherMonitoringAndReportingService.Bots;
 using WeatherMonitoringAndReportingService.Config;
 using WeatherMonitoringAndReportingService.DataSourceProcessor;
 using WeatherMonitoringAndReportingService.InputConversion;
+using WeatherMonitoringAndReportingService.WeatherDetails;
 
 namespace WeatherMonitoringAndReportingService
 {
@@ -12,6 +13,9 @@ namespace WeatherMonitoringAndReportingService
         static void Main(string[] args)
         {
             InitializeApp();
+
+            Console.WriteLine("Please choose input format:\n1. JSON\n2. XML");
+            int choice = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter weather status (type 'STOP' to finish):");
 
@@ -24,7 +28,22 @@ namespace WeatherMonitoringAndReportingService
                 userInput.AppendLine(line);
             }
 
-            _weatherStation.Notify(JSONToWeatherDetailsAdapter.ToWeatherDetailsAdapter(userInput.ToString()));
+            WeatherDetailsModel weatherDetails = new();
+
+            switch (choice)
+            {
+                case 1:
+                    weatherDetails = JSONToWeatherDetailsAdapter.ToWeatherDetailsAdapter(userInput.ToString());
+                    break;
+                case 2:
+                    weatherDetails = XMLToWeatherDetailsAdapter.ToWeatherDetailsAdapter(userInput.ToString());
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice!");
+                    break;
+            }
+
+            _weatherStation.Notify(weatherDetails);
         }
 
         public static void InitializeApp()
