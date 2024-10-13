9639f405932eba01a2a64c5d10682e714c4f4d08
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-10-13 08:02:33 +0200

    XML deserializer unit tests

--- WeatherMonitoringAndReportingService.Tests/InputConversion/XMLToWeatherDetailsAdapterTests.cs
+++ WeatherMonitoringAndReportingService.Tests/InputConversion/XMLToWeatherDetailsAdapterTests.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoFixture;
+using FluentAssertions;
+using WeatherMonitoringAndReportingService.InputConversion;
+using WeatherMonitoringAndReportingService.WeatherDetails;
 
-namespace WeatherMonitoringAndReportingService.Tests.InputConversion
+namespace WeatherMonitoringAndReportingService.Tests.InputConversion;
+
+public class XMLToWeatherDetailsAdapterTests
 {
-    internal class XMLToWeatherDetailsAdapterTests
+    private readonly Fixture _fixture;
+
+    public XMLToWeatherDetailsAdapterTests()
     {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void ToWeatherDetailsAdapter_ShouldReturnWeatherDetailsModel()
+    {
+        // Arrange
+        var xmlInput = @"<WeatherData>
+                            <Location>City Name</Location>
+                            <Temperature>23.0</Temperature>
+                            <Humidity>85.0</Humidity>
+                        </WeatherData>";
+
+        // Act
+        var weatherDetailsMode = XMLToWeatherDetailsAdapter.ToWeatherDetailsAdapter(xmlInput);
+
+        // Assert
+        weatherDetailsMode.Should().BeOfType<WeatherDetailsModel>();
+    }
+
+    [Fact]
+    public void ToWeatherDetailsAdapter_Fail_ThrowException_WhenInputIsInvalidXML()
+    {
+        // Arrange
+
+        // Act
+        Action weatherDetails = () => XMLToWeatherDetailsAdapter.ToWeatherDetailsAdapter(_fixture.Create<string>());
+
+        // Assert
+        weatherDetails.Should().Throw<InvalidOperationException>();
     }
 }
